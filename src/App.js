f99a729f51d8893fc98678beedbdc897807d949a
aumoor02 <109328140+aumoor02@users.noreply.github.com>
2023-10-13 15:56:53 -0500

    Initialized Footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./components/About";
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
     return (
@@ -16,6 +17,9 @@ function App() {
             <Skills />
             <Projects />
             <Contact />
+            <footer>
+                <Footer />
+            </footer>
         </div>
     );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      Footer (Icons with links)
+    </div>
+  )
+}
+
+export default Footer
