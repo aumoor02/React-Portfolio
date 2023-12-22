a41af6098ccc2a1e16472eb67d258f36f8e4598c
aumoor02 <109328140+aumoor02@users.noreply.github.com>
2023-12-22 00:30:30 -0600

    Changed Skills, Reordered Layout, Added Rocket Icon

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,38 +1,45 @@
 import React from "react";
 import CodingSkills from "../assets/coding-skills.jpg";
+import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
 
 
 function Skills() {
     return (
         <div className="skills-container">
-            <h2>Skills</h2>
+            <h2 className="skills-heading">Skills</h2>
             <div className="skills-list">
                 <div className="skill">
-                    
+                    <RocketLaunchIcon/> HTML/CSS
                 </div>
                 <div className="skill">
-                    
+                    <RocketLaunchIcon/> JavaScript
                 </div>
                 <div className="skill">
-                    
+                    <RocketLaunchIcon/> Bootstrap
                 </div>
                 <div className="skill">
-                    
+                    <RocketLaunchIcon/> React.js
                 </div>
                 <div className="skill">
-                    
+                    <RocketLaunchIcon/> React Native
                 </div>
                 <div className="skill">
-                    
+                    <RocketLaunchIcon/> Node.js
                 </div>
                 <div className="skill">
-                    
+                    <RocketLaunchIcon/> Express.js
                 </div>
                 <div className="skill">
-                    
+                    <RocketLaunchIcon/> MongoDB
                 </div>
                 <div className="skill">
-                    
+                    <RocketLaunchIcon/> Python
+                </div>
+                <div className="skill">
+                    <RocketLaunchIcon/> PostgreSQL
+                </div>
+                <div className="skill">
+                    <RocketLaunchIcon/> Git/Github
                 </div>
             </div>
             <img
