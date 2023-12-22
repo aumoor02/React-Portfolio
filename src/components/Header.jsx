5fb23947afef4a5bc0f5017f37374fc9a5eb0f54
aumoor02 <109328140+aumoor02@users.noreply.github.com>
2023-12-22 00:47:22 -0600

    Changed Arrow On Learn More Button to Single

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,32 @@
 import React from "react";
 import Navigation from "./Navigation";
-import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
-function Header({scrollRef}) {
-    const scrollDown = (e) => {
-        e.current.scrollIntoView({
-            behavior: "smooth"
-        });
-    };
+function Header({ scrollRef }) {
+  const scrollDown = (e) => {
+    e.current.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
 
-    return (
-        <div className="header">
-            <Navigation scrollRef={scrollRef}/>
-            <div className="header-description">
-                <h1>Austin Moore</h1>
-                <p>
-                    I am a web developer who uses front-end technologies <br />
-                    to bring web pages to life!
-                </p>
-            </div>
-            <button onClick={() => scrollDown(scrollRef)} className="header-btn">
-                Learn More
-                <div><KeyboardDoubleArrowDownIcon/></div>
-            </button>
+  return (
+    <div className="header">
+      <Navigation scrollRef={scrollRef} />
+      <div className="header-description">
+        <h1>Austin Moore</h1>
+        <p>
+          I am a web developer who uses front-end technologies <br />
+          to bring web pages to life!
+        </p>
+      </div>
+      <button onClick={() => scrollDown(scrollRef)} className="header-btn">
+        Learn More
+        <div>
+          <KeyboardArrowDownIcon />
         </div>
-    );
+      </button>
+    </div>
+  );
 }
 
 export default Header;
